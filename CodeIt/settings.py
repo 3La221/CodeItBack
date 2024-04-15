8dd32c638d711cbfc2f3de8a321b469e0319d0c2
3La221 <abacha3la2@gmail.com>
2024-04-15 12:30:54 +0100

    'resolve_static_prob'

--- CodeIt/settings.py
+++ CodeIt/settings.py
@@ -137,7 +137,7 @@ import os
 STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = os.path.join(BASE_DIR, "")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
