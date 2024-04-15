cb964fba8bfc6dbc0d6d68c4ab620fa4843435c5
3La221 <abacha3la2@gmail.com>
2024-04-15 12:32:28 +0100

    'ff'

--- CodeIt/settings.py
+++ CodeIt/settings.py
@@ -137,7 +137,12 @@ import os
 STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS = os.path.join(BASE_DIR, "")
+if DEBUG:
+        STATICFILES_DIRS = [
+            os.path.join(BASE_DIR, 'static')]
+else:
+        STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
